5109a7b44968a5b5d5e165ac2c2f84fadfa78d2e
pablobhn <pablobhn@gmail.com>
2022-06-28 01:54:36 -0300

    merger a mano cambios cristian (#6)

---
+++ js/header.js
@@ -0,0 +1,27 @@
+var pathname = window.location.pathname;
+
+function make(){
+    if (pathname.includes("/index.html")){
+        var element = document.getElementById("menu1");
+        element.classList.add("actual");
+    }
+    if (pathname.includes("/institucional.html")){
+        var element = document.getElementById("menu2");
+        element.classList.add("actual");
+    }
+    if (pathname.includes("/novedades.html")){
+        var element = document.getElementById("menu3");
+        element.classList.add("actual");
+    }
+    if (pathname.includes("/contacto.html")){
+        var element = document.getElementById("menu4");
+        element.classList.add("actual");
+    }
+    if (pathname.includes("/streaming.html")){
+        var element = document.getElementById("menu5");
+        element.classList.add("actual");
+    }
+
+
+}
+
